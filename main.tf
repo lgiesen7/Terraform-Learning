4c0176d4eadb5fb27f6e89436b623d986bd0da8c
lgiesen7 <115139074+lgiesen7@users.noreply.github.com>
2022-10-12 08:27:02 +0200

    wiped and rebuilt

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-terraform {
-  required_providers {
-    vsphere = {
-      source = "hashicorp/vsphere"
-      version = "2.0.2"
-    }
-  }
-}
-provider "vsphere" {
-  user              = var.user
-  password          = var.password
-  vsphere_server    = var.vsphere_server
-
-  allow_unverified_ssl = true
+resource "random_string" "test" {
+  length = 10
 }
